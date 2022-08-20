2b2a0c39c84550cba52472e1aaf99104468732d7
yusuf-mutlu <yusufmutlu88@gmail.com>
2022-08-20 22:10:04 +0300

    schedule SaveCoinPricesFromCoingecko command to run every five minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(UpdateDataFromCoinGecko::Class)->everyFourHours();
-        $schedule->command(SaveCoinPricesFromCoinGecko::Class)->everyThreeMinutes();
+        $schedule->command(SaveCoinPricesFromCoinGecko::Class)->everyFiveMinutes();
     }
 
     /**
